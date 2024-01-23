605a77a816722b7bdd79651cba5c200c67637424
PEDRO  PRATES <157413476+PedroPrates08@users.noreply.github.com>
2024-01-23 14:05:03 -0300

    Add files via upload

---
+++ e.py
@@ -0,0 +1,10 @@
+#Escreva um programa que compare se duas sequências digitadas pelo usuário são iguais.   
+
+seq1 = input("Digite sua sequência: ")
+seq2 = input("Digite sua outra sequência: ")
+
+if seq1 == seq2:
+    print("IGUAl!!!")
+
+else:
+    print("DIFERENTE!!!")
